431da54e9c5353db353884229b53eb17093ad0da
Shubham Kumar <shubhamkumarfb90@gmail.com>
2025-08-31 18:34:52 +0530

    Fixed the gallery page

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -26,16 +26,25 @@ export default function GalleryPage() {
 
   return (
     <div>
-      <div className="bg-[#fafafa] min-h-screen flex flex-col">
-        <Navbar />
+      <div className="bg-[#fafafa] min-h-screen flex flex-col pt-20">
+  <Navbar />
+
 
         {/* Spline Section */}
-        <div className="w-full h-screen relative">
-          <Spline
-            scene="https://prod.spline.design/u-FE8axZ14n8nwcH/scene.splinecode"
-            className="w-full h-full"
-          />
-        </div>
+        
+<div className="hidden sm:block w-full relative">
+  <div className="w-full h-[70vh] lg:h-screen">
+    <Spline
+      scene="https://prod.spline.design/u-FE8axZ14n8nwcH/scene.splinecode"
+      className="w-full h-full"
+    />
+  </div>
+</div>
+
+
+
+
+
 
         {/* Gallery Section */}
         <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-[#fafafa] relative z-10">
